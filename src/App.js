d0fd5b2f1d53d0d5b29c747c3ca848121e082841
Pandey <703250079@genpact.com>
2022-01-06 11:35:53 +0530

    change app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-	  Hello my Friend, how r you
+	  Hello my Friend, i am changed in branch
         </p>
         <a
           className="App-link"
